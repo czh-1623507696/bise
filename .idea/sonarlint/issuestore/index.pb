5031ba31e920d6a9fb84334a320c7da3ff567d4e
czh <1623507696@qq.com>
2022-04-19 09:43:25 +0800

    邮件实现

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,6 @@
 
 4
 .env,3\c\3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
-<
-microblog.py,8\6\8650f32b79892e22f1453a34dff7823b34a45d3d
 K
 app/templates/register.html,8\2\8271b8723a0435c43160a565f02704ca4b22486e
 =
--- app/__init__.py
+++ app/__init__.py
@@ -6,14 +6,17 @@ from config import Config
 
 from flask_sqlalchemy import SQLAlchemy#从包中导入类
 from flask_migrate import Migrate
-
+from flask_mail import Mail
 from flask_login import LoginManager
 from logging.handlers import RotatingFileHandler
+from flask_bootstrap import Bootstrap
 import os
 app = Flask(__name__)#将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
 app.config.from_object(Config)
 #print('等会谁（哪个包或模块）在使用我：',__name__)
 
+mail = Mail(app)
+bootstrap = Bootstrap(app)
 login = LoginManager(app)
 login.login_view = 'login'
 db = SQLAlchemy(app)#数据库对象
